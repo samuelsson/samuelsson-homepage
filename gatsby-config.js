38a67a15761ea6003ffc5b64e3349d5316dd5be1
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2019-10-08 18:49:10 +0200

    feat: 🎸 add timeline component for about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,27 @@ module.exports = {
       linkedIn: 'eriksamuelsson',
     },
     baseUrl: 'https://eriksamuelsson.com',
+    aboutMeTimeLine: [
+      { year: 1988, text: 'Yaay! I was born.' },
+      { year: 1994, text: 'My dad buys our first computer and I am hooked.' },
+      {
+        year: 2001,
+        text: 'I start building my own homepages, a lot of table elements.',
+      },
+      { year: 2007, text: 'Buy my first Mac before even using one ever.' },
+      { year: 2008, text: 'Start working in an electronic retail store.' },
+      {
+        year: 2010,
+        text: 'I launch a quite popular iPhone blog built on WordPress.',
+      },
+      { year: 2011, text: 'I start my own company, doing web design.' },
+      { year: 2012, text: 'I join Mensa Sweden.' },
+      { year: 2015, text: 'I graduate from the University.' },
+      {
+        year: 2016,
+        text: 'I start working professionally as a web developer.',
+      },
+    ],
   },
   plugins: [
     'gatsby-plugin-sharp',
