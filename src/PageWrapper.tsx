a76861cadf4f86a01d55b64b700c17c97dfa6b5e
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2020-10-14 16:56:00 +0200

    refactor: 💡 change from typefaces to fontsource

--- src/PageWrapper.tsx
+++ src/PageWrapper.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { WrapPageElementBrowserArgs } from 'gatsby';
 import { Normalize } from 'styled-normalize';
-import 'typeface-montserrat';
+import 'fontsource-montserrat/latin.css';
 import GlobalStyles from './GlobalStyles';
 
 const PageWrapper = ({ element }: WrapPageElementBrowserArgs): JSX.Element => (
